fddbe17a704264baa1c1992e0b9088cde0316566
Alejandro Martínez <141962349+Lualmara-5@users.noreply.github.com>
2023-10-14 17:53:08 -0500

    Add Buttons

---
+++ Dark_Mode_Button#1/js/scripts.js
@@ -0,0 +1,9 @@
+function Modo_Oscuro() {
+  document.body.classList.add("Fondo_Oscuro");
+  document.body.classList.remove("Fondo_Claro");
+}
+
+function Modo_Claro() {
+  document.body.classList.add("Fondo_Claro");
+  document.body.classList.remove("Fondo_Oscuro");
+}
